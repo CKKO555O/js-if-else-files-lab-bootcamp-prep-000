07702d0a0bb694ee5cec1b4d02dcc6121ed09a41
Charles  Ohene <ckumi7@gmail.com>
2018-08-05 00:18:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var name = "Joe"
-var height = "74"
+var height = "70"
 
 if (name === "Joe") {
     console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
 }
 
-if (height === 74) {
+if (height === "70") {
     console.log("The height is correct")
+} else {
+    console.log("Expected different height")
 }
